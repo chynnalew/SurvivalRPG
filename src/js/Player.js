4ffb0cb69ad06d139993f18b8852e09dd8de0ad7
chynnalew <chynnalew@yahoo.com>
2021-12-01 10:57:11 -0800

    refactor player constructor into seperate js

---
+++ src/js/Player.js
@@ -0,0 +1,69 @@
+import Phaser from '../lib/phaser.js';
+
+export default class Player extends Phaser.Physics.Matter.Sprite {
+  constructor(data) {
+    let { scene, x, y, texture, frame } = data;
+    super(scene.matter.world, x, y, texture, frame);
+    this.animations();
+    this.scene.add.existing(this);
+  }
+
+  static preload(scene) {
+    scene.load.spritesheet('playerFront', '../assets/player/front.png', { frameHeight: 32, frameWidth: 32 });
+    scene.load.spritesheet('playerBack', '../assets/player/back.png', { frameHeight: 32, frameWidth: 32 });
+    scene.load.spritesheet('playerRight', '../assets/player/right.png', { frameHeight: 32, frameWidth: 32 });
+    scene.load.spritesheet('playerLeft', '../assets/player/left.png', { frameHeight: 32, frameWidth: 32})
+  }
+
+  animations() {
+    this.scene.anims.create({
+      key: 'walkDown',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNumbers('playerFront',{frames:[0,1,2,3]})
+    });
+    this.scene.anims.create({
+      key: 'walkUp',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNumbers('playerBack',{frames:[0,1,2,3]})
+    });
+    this.scene.anims.create({
+      key: 'walkRight',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNumbers('playerRight',{frames:[0,1,2,3]})
+    });
+    this.scene.anims.create({
+      key: 'walkLeft',
+      frameRate: 10,
+      repeat: -1,
+      frames: this.scene.anims.generateFrameNumbers('playerLeft',{frames:[0,1,2,3]})
+    })
+  }
+
+  update() {
+    
+    const speed = 2.5;
+    let playerVelocity = new Phaser.Math.Vector2();
+
+    if (this.inputKeys.left.isDown || this.inputKeys.left2.isDown) {
+      this.anims.play('walkLeft', true);
+      playerVelocity.x = -1;
+    } else if (this.inputKeys.right.isDown || this.inputKeys.right2.isDown) {
+      this.anims.play('walkRight', true);
+      playerVelocity.x = 1;
+    }
+    if (this.inputKeys.up.isDown || this.inputKeys.up2.isDown) {
+      this.anims.play('walkUp', true);
+      playerVelocity.y = -1;
+    } else if (this.inputKeys.down.isDown || this.inputKeys.down2.isDown) {
+      this.anims.play('walkDown', true);
+      playerVelocity.y = 1;
+    }
+
+    playerVelocity.normalize();
+    playerVelocity.scale(speed);
+    this.setVelocity(playerVelocity.x, playerVelocity.y)
+  }
+}
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,4 +1,5 @@
 import Phaser from '../lib/phaser.js';
+import Player from '../js/Player.js';
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
@@ -6,17 +7,13 @@ export default class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet('playerFront', '../assets/player/front.png', { frameHeight: 32, frameWidth: 32 });
-    this.load.spritesheet('playerBack', '../assets/player/back.png', { frameHeight: 32, frameWidth: 32 });
-    this.load.spritesheet('playerRight', '../assets/player/right.png', { frameHeight: 32, frameWidth: 32 });
-    this.load.spritesheet('playerLeft', '../assets/player/left.png', { frameHeight: 32, frameWidth: 32})
+    Player.preload(this);
   }
     
   create() {
-    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world, this.game.renderer.width / 2, this.game.renderer.height / 2, 'playerFront', 0);
-    this.add.existing(this.player);
+    this.player = new Player({ scene: this, x: this.game.renderer.width / 2, y: this.game.renderer.height / 2, texture: 'playerFront', frame: 0});
 
-    this.inputKeys = this.input.keyboard.addKeys({
+    this.player.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
@@ -27,53 +24,34 @@ export default class MainScene extends Phaser.Scene {
       right2: Phaser.Input.Keyboard.KeyCodes.RIGHT,
     })
 
-    this.anims.create({
-      key: 'walkDown',
-      frameRate: 10,
-      repeat: -1,
-      frames: this.anims.generateFrameNumbers('playerFront',{frames:[0,1,2,3]})
-    });
-    this.anims.create({
-      key: 'walkUp',
-      frameRate: 10,
-      repeat: -1,
-      frames: this.anims.generateFrameNumbers('playerBack',{frames:[0,1,2,3]})
-    });
-    this.anims.create({
-      key: 'walkRight',
-      frameRate: 10,
-      repeat: -1,
-      frames: this.anims.generateFrameNumbers('playerRight',{frames:[0,1,2,3]})
-    });
-    this.anims.create({
-      key: 'walkLeft',
-      frameRate: 10,
-      repeat: -1,
-      frames: this.anims.generateFrameNumbers('playerLeft',{frames:[0,1,2,3]})
-    })
+  
+    // this.anims.create({
+    //   key: 'walkDown',
+    //   frameRate: 10,
+    //   repeat: -1,
+    //   frames: this.anims.generateFrameNumbers('playerFront',{frames:[0,1,2,3]})
+    // });
+    // this.anims.create({
+    //   key: 'walkUp',
+    //   frameRate: 10,
+    //   repeat: -1,
+    //   frames: this.anims.generateFrameNumbers('playerBack',{frames:[0,1,2,3]})
+    // });
+    // this.anims.create({
+    //   key: 'walkRight',
+    //   frameRate: 10,
+    //   repeat: -1,
+    //   frames: this.anims.generateFrameNumbers('playerRight',{frames:[0,1,2,3]})
+    // });
+    // this.anims.create({
+    //   key: 'walkLeft',
+    //   frameRate: 10,
+    //   repeat: -1,
+    //   frames: this.anims.generateFrameNumbers('playerLeft',{frames:[0,1,2,3]})
+    // })
   }
 
   update() {
-    const speed = 2.5;
-    let playerVelocity = new Phaser.Math.Vector2();
-
-    if (this.inputKeys.left.isDown || this.inputKeys.left2.isDown) {
-      this.player.anims.play('walkLeft', true);
-      playerVelocity.x = -1;
-    } else if (this.inputKeys.right.isDown || this.inputKeys.right2.isDown) {
-      this.player.anims.play('walkRight', true);
-      playerVelocity.x = 1;
-    }
-    if (this.inputKeys.up.isDown || this.inputKeys.up2.isDown) {
-      this.player.anims.play('walkUp', true);
-      playerVelocity.y = -1;
-    } else if (this.inputKeys.down.isDown || this.inputKeys.down2.isDown) {
-      this.player.anims.play('walkDown', true);
-      playerVelocity.y = 1;
-    }
-
-    playerVelocity.normalize();
-    playerVelocity.scale(speed);
-    this.player.setVelocity(playerVelocity.x, playerVelocity.y)
+    this.player.update();
   }
 }
